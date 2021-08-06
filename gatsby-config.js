548d2ae4b157360541073502017c28033c9833ef
jlavila13 <jose.leon.avila@gmail.com>
2021-08-05 22:48:50 -0400

    updated config

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: "",
+        trackingId: "UA-77595003-1",
       },
     },
     "gatsby-plugin-react-helmet",
